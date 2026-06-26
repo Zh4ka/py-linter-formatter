82517dabb14ffebae10984278abff9ae72ffaf67
Zheka <evgenijsturmak@gmail.com>
2026-06-26 05:22:23 +0300

    correcting a coding style error N2 add ","

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def format_linter_error(error: dict) -> dict:
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
-        "source": "flake8"
+        "source": "flake8",
     }
 
 
